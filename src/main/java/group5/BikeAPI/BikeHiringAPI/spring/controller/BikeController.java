cd05bf0d077f7326cfac012481a91b70d5fb6296
tramanh9999 <anhthtse141171@fpt.edu.vn>
2019-06-28 12:38:41 +0700

    test connet db

--- src/main/java/group5/BikeAPI/BikeHiringAPI/spring/controller/BikeController.java
+++ src/main/java/group5/BikeAPI/BikeHiringAPI/spring/controller/BikeController.java
@@ -22,7 +22,7 @@ public class BikeController {
     @Autowired
     BikeService BikeService;
 
-    @CrossOrigin(origins = "http://localhost:63342", allowCredentials = "true")
+    @CrossOrigin( allowCredentials = "true")
     @ApiOperation(value = "Retrieve all bikes")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "All bike retrieved successfully"), @ApiResponse(code =
             401, message = "Not authorized to access this data"), @ApiResponse(code = 403, message = "This data is forbidden"), @ApiResponse(code = 404, message = "This resource is not found")})
